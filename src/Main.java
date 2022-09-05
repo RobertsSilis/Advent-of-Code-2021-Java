e706708d24d922b4b5e139980e564237835cf5ec
RobertsSilis <roberts.siilis@gmail.com>
2022-09-05 21:31:25 +0300

    Commit nr 2

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,7 @@ import java.util.ArrayList; //Allows using ArrayList type
  * 
  * Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; 
  * the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+ * Extra comment
  */
 
 public class Main 
