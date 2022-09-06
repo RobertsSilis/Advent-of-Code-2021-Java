67bd66e86fee289908221b67543bd4a1193dce9f
RobertsSilis <roberts.siilis@gmail.com>
2022-09-06 07:51:09 +0300

    Moved function from main to DayOne

--- src/DayOne.java
+++ src/DayOne.java
@@ -1,8 +1,33 @@
+import java.io.File; //Allows managing files
+import java.io.FileNotFoundException; //Catches exception
+import java.util.Scanner; //Allows reading from a file
 import java.util.ArrayList; //Allows using ArrayList type
 
 
 public class DayOne 
 {
+	static void dayOne()
+	{
+		try 
+		{
+			File numbers = new File("src\\Input.txt");  
+			Scanner myReader = new Scanner(numbers);
+			ArrayList<Integer> input = new ArrayList<Integer>();  
+			while(myReader.hasNextLine())
+			{
+				input.add(Integer.parseInt(myReader.nextLine()));  //Writes the input file into variable "input"
+			}
+			System.out.println(DayOne.taskOne(input)); //Prints out the solution to day 1 task 1 problem.
+			System.out.println(DayOne.taskTwo(input)); //Prints out the solution to day 1 task 2 problem.
+			myReader.close();
+			input.clear();
+		} 
+		catch (FileNotFoundException e) 
+		{
+		     System.out.println("An error occurred.");
+		     e.printStackTrace();
+		}
+	}
 	/*
 	 * As the submarine drops below the surface of the ocean, it automatically performs a sonar sweep of the nearby sea floor. 
 	 * On a small screen, the sonar sweep report (your puzzle input) appears: 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,3 @@
-import java.io.File; //Allows managing files
-import java.io.FileNotFoundException; //Catches exception
-import java.util.Scanner; //Allows reading from a file
-import java.util.ArrayList; //Allows using ArrayList type
 
 /*
  * You're minding your own business on a ship at sea when the overboard alarm goes off! 
@@ -23,24 +19,6 @@ public class Main
 {
 	public static void main(String[] args) 
 	{
-		try 
-		{
-			File numbers = new File("src\\Input.txt");  
-			Scanner myReader = new Scanner(numbers);
-			ArrayList<Integer> input = new ArrayList<Integer>();  
-			while(myReader.hasNextLine())
-			{
-				input.add(Integer.parseInt(myReader.nextLine()));  //Writes the input file into variable "input"
-			}
-			System.out.println(DayOne.taskOne(input)); //Prints out the solution to day 1 task 1 problem.
-			System.out.println(DayOne.taskTwo(input)); //Prints out the solution to day 1 task 2 problem.
-			myReader.close();
-			input.clear();
-		} 
-		catch (FileNotFoundException e) 
-		{
-		     System.out.println("An error occurred.");
-		     e.printStackTrace();
-		}
+		DayOne.dayOne();
 	}
 }
